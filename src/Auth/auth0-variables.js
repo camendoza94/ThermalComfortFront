2ba07e327ee3543d7f72cf89e7f91941c99e00d9
camendoza94 <ca.mendoza968@uniandes.edu.co>
2017-09-27 13:26:50 -0500

    Auth0 template

---
+++ src/Auth/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+  domain: 'arquisoft20172.auth0.com',
+  clientId: 'fynjUzmxrw2sGQCK5LHiRBOOKh7FMbIu',
+  callbackUrl: 'http://localhost:3000/callback',
+  apiUrl: 'uniandes.edu.co/thermalcomfort'
+}
